a076490a5f413111a46721e83e7d852d98d28896
CIC Studios <110351853+cic-studios@users.noreply.github.com>
2024-10-18 23:39:05 +0200

    restructure & validations

---
+++ App/app_Intro&Login.js
@@ -0,0 +1,275 @@
+document.addEventListener("readystatechange", function(){ if (document.readyState === "complete"){GetHash();} });
+
+function RestartIntroAnimations()
+{
+    const delay = 50;
+    document.getElementById("IntroLogosCONTAINER").style.display = "flex";
+    window.setTimeout(function(){document.getElementById("IntroLogosCONTAINER").style.display = "none";}, 10000);
+    window.setTimeout(function(){document.getElementById("IntroLoadingTXT").style.display = "none";}, 250);
+
+    //RestartAnimationClassDelay("IntroLogosCONTAINER", "logoDisable_ANIM", delay, 1, 1);
+    RestartAnimationClassDelay("IntroBackgroundDIV", "cicBackground_ANIM", delay, 1, 1);
+    //RestartAnimationClassDelay("IntroLoadingTXT", "cicLoading_ANIM", delay, 1, 1);
+    RestartAnimationClassDelay("IntroHeartIMG", "cicHeart_ANIM", delay, 0, 0);
+    RestartAnimationClassDelay("IntroCubeIMG", "cicCube_ANIM", delay, 0, 1);
+    RestartAnimationClassDelay("IntroEyesIMG", "cicEyes_ANIM", delay, 0, 0);
+    RestartAnimationClassDelay("IntroCatInCubeIMG", "cicCatInCube_ANIM", delay, 0, 1);
+    RestartAnimationClassDelay("IntroStudiosIMG", "cicStudios_ANIM", delay, 0, 1);
+    RestartAnimationClassDelay("IntroWhiteCicIMG", "cicWhite_ANIM", delay, 0, 0);
+    RestartAnimationClassDelay("IntroPurpleIMG", "mbaPurple_ANIM", delay, 0, 1);
+    RestartAnimationClassDelay("IntroColorIMG", "mbaColor_ANIM", delay, 0, 1);
+    RestartAnimationClassDelay("IntroTextIMG", "mbaText_ANIM", delay, 0, 0);
+    RestartAnimationClassDelay("IntroWhiteMbaIMG", "mbaWhite_ANIM", delay, 0, 0);
+    
+}
+function RestartAnimationClassDelay(elementName, className, delay=50, waitOpacity=1, startOpacity=1)
+{
+    const element = document.getElementById(elementName);
+    element.classList.remove(className);
+    element.style.opacity = waitOpacity;
+    window.setTimeout(function(){RestartAnimationClassDelayFinalize(element, className, startOpacity);}, delay);
+}
+function RestartAnimationClassDelayFinalize(element, className, startOpacity)
+{
+    element.style.opacity = startOpacity;
+    element.classList.add(className);
+}
+
+function GetHash()
+{
+    const urlParams = new URLSearchParams(window.location.hash.slice(1));
+    const user = urlParams.get('u');
+    const pass = urlParams.get('p');
+    const sess = urlParams.get('s');
+    const intro = urlParams.get('i');
+
+    if(intro && (intro=="f" || intro=="false")) 
+        document.getElementById("IntroLogosCONTAINER").style.display = "none";
+    else
+        RestartIntroAnimations();
+
+    if(user) 
+    {
+        const usernameField = document.getElementById("username");
+        if(usernameField)
+        {
+            usernameField.value = user; 
+            DisableElementSelection(usernameField, true);
+        }
+    }
+    if(pass)
+    {
+        const passwordField = document.getElementById("password");
+        if(passwordField)
+        {
+            passwordField.value = pass;
+            DisableElementSelection(passwordField, true);
+        }
+    }
+    if(sess) 
+    {
+        const sessionIDField = document.getElementById("sessionID");
+        if(sessionIDField)
+        {
+            sessionIDField.value = sess; 
+            DisableElementSelection(sessionIDField, true);
+        }
+        const serverIDField = document.getElementById("serverID");
+        if(serverIDField)
+        {
+            serverIDField.value = sess.split(":")[0]; 
+            DisableElementSelection(serverIDField, true);
+        }
+    }
+}
+function DisableElementSelection(element, changeAspect=false, forceUserSelectNone=false)
+{
+    element.tabIndex = -1;
+    element.setAttribute('readonly', 'true');
+    element.setAttribute('unselectable', 'on');
+    element.classList.remove('userSelectAuto');
+    element.classList.remove('userSelectContain');
+    if(forceUserSelectNone) {element.style.userSelect = "none";}
+    element.onselect = function(event) {event.preventDefault(); this.selectionStart = this.selectionEnd; window.getSelection().removeAllRanges(); return false;};
+    element.onselectstart = function(event) {event.preventDefault(); this.selectionStart = this.selectionEnd; window.getSelection().removeAllRanges(); return false;};
+    element.onselectend = function(event) {event.preventDefault(); this.selectionStart = this.selectionEnd; window.getSelection().removeAllRanges(); return false;};
+    element.oncopy = function(event) {event.preventDefault(); return false;};
+    element.oncut = function(event) {event.preventDefault(); return false;};
+    element.onpaste = function(event) {event.preventDefault(); return false;};
+    element.oncontextmenu = function(event) {event.preventDefault(); return false;};
+    element.onmousedown = function(event) {event.preventDefault(); return false;};
+    element.onkeydown = function(event) {event.preventDefault(); return false;};
+    element.onclick = function(event) {event.preventDefault(); return false;};
+    element.ondblclick = function(event) {event.preventDefault(); return false;};
+    element.onfocus = function(event) {event.preventDefault(); this.blur(); return false;};
+    if(changeAspect)
+    {
+        element.style.background = "transparent";
+        element.style.fontStyle = "italic";
+        element.style.color = "var(--mbaInputIndigo)";
+    }
+}
+
+
+
+function click_ShowSessionLogin()
+{
+    document.getElementById("createAccountUI").style.display = "none";
+    document.getElementById("resetPasswordUI").style.display = "none";
+    document.getElementById("loginUI").style.display = "flex";
+}
+function click_ShowResetPassword()
+{
+    document.getElementById("loginUI").style.display = "none";
+    document.getElementById("createAccountUI").style.display = "none";
+    document.getElementById("resetPasswordUI").style.display = "flex";
+}
+function click_ShowCreateAccount()
+{
+    document.getElementById("loginUI").style.display = "none";
+    document.getElementById("resetPasswordUI").style.display = "none";
+    document.getElementById("createAccountUI").style.display = "flex";
+}
+
+
+
+function PerformElementFieldCheck(element, msgArticle, msgNoun)
+{
+    if(!element.value)
+    {
+        cicMSG_ShowMessage(copyrightDiv, "Please enter " + msgArticle + " " + msgNoun + ".", copyrightMsg, 5, 0.5, 'red', 'var(--mbaInputIndigo)', 'var(--mbaDarkPurple)');
+        return false;
+    }
+    element.value = cicCutAndTrim(element.value);
+    if(element.length < 1)
+    {
+        cicMSG_ShowMessage(copyrightDiv, "Invalid " + msgNoun + ".", copyrightMsg, 5, 0.5, 'red', 'var(--mbaInputIndigo)', 'var(--mbaDarkPurple)');
+        return false;
+    }
+    return true;
+}
+
+function click_SessionLogin()
+{
+    const sessionIDField = document.getElementById("sessionID");
+    const usernameField = document.getElementById("username");
+    const passwordField = document.getElementById("password");
+
+    if(PerformElementFieldCheck(sessionIDField, "a", "session ID") == false) return;
+    const sessionIDparts = sessionIDField.value.split(":");
+    if (sessionIDparts.length !== 3) {cicMSG_ShowMessage(copyrightDiv, "Invalid session ID.", copyrightMsg, 5, 0.5, 'red', 'var(--mbaInputIndigo)', 'var(--mbaDarkPurple)'); return;}
+    if(PerformElementFieldCheck(usernameField, "your", "username") == false) return;
+    if(PerformElementFieldCheck(passwordField, "your", "password") == false) return;
+
+    const sessionLoginData = 
+    {
+        serverID: sessionIDparts[0],
+        sessionID: sessionIDparts[1],
+        sessionNum: sessionIDparts[2],
+        username: usernameField.value,
+        password: passwordField.value
+    };
+}
+
+
+
+function click_CreateAccount()
+{
+    const sessionIDField = document.getElementById("sessionID");
+    const createAccountEmailField = document.getElementById("createAccountEmail");
+    const createAccountUsernameField = document.getElementById("createAccountUsername");
+    const createAccountDisplaynameField = document.getElementById("createAccountDisplayname");
+    const createAccountPasswordField = document.getElementById("createAccountPassword");
+    const createAccountConfirmPasswordField = document.getElementById("createAccountConfirmPassword");
+
+    if(PerformElementFieldCheck(sessionIDField, "a", "session ID") == false) return;
+    const sessionIDparts = sessionIDField.value.split(":");
+    if (sessionIDparts.length<1 || sessionIDparts[0].length<1) {cicMSG_ShowMessage(copyrightDiv, "Invalid session ID.", copyrightMsg, 5, 0.5, 'red', 'var(--mbaInputIndigo)', 'var(--mbaDarkPurple)'); return;}
+    if(PerformElementFieldCheck(createAccountEmailField, "your", "email") == false) return;
+    if(PerformElementFieldCheck(createAccountUsernameField, "your", "username") == false) return;
+    if(PerformElementFieldCheck(createAccountDisplaynameField, "your", "display name") == false) return;
+    if(PerformElementFieldCheck(createAccountPasswordField, "your", "password") == false) return;
+    if(!createAccountConfirmPasswordField.value) {cicMSG_ShowMessage(copyrightDiv, "Please confirm your password.", copyrightMsg, 5, 0.5, 'red', 'var(--mbaInputIndigo)', 'var(--mbaDarkPurple)'); return;}
+    createAccountConfirmPasswordField.value = cicCutAndTrim(createAccountConfirmPasswordField.value);
+    if(createAccountConfirmPasswordField != createAccountPasswordField) {cicMSG_ShowMessage(copyrightDiv, "Passwords do not match.", copyrightMsg, 5, 0.5, 'red', 'var(--mbaInputIndigo)', 'var(--mbaDarkPurple)'); return;}
+  
+    const createAccountData = 
+    {
+        serverID: sessionIDparts[0],
+        sessionID: sessionIDparts[1],
+        sessionNum: sessionIDparts[2],
+        email: createAccountEmailField.value,
+        username: createAccountUsernameField.value,
+        password: createAccountPasswordField.value,
+        displayname: createAccountDisplaynameField.value,
+        confirmPassword: createAccountConfirmPasswordField.value
+    };
+}
+
+function click_SendUsername()
+{
+    const sessionIDField = document.getElementById("sessionID");
+    const recoveryEmailField = document.getElementById("recoveryEmail");
+
+    if(PerformElementFieldCheck(sessionIDField, "a", "session ID") == false) return;
+    const sessionIDparts = sessionIDField.value.split(":");
+    if (sessionIDparts.length<1 || sessionIDparts[0].length<1) {cicMSG_ShowMessage(copyrightDiv, "Invalid session ID.", copyrightMsg, 5, 0.5, 'red', 'var(--mbaInputIndigo)', 'var(--mbaDarkPurple)'); return;}
+    if(PerformElementFieldCheck(recoveryEmailField, "your", "email") == false) return;
+
+    const recoverUsernameData = 
+    {
+        serverID: sessionIDparts[0],
+        sessionID: sessionIDparts[1],
+        sessionNum: sessionIDparts[2],
+        email: recoveryEmailField.value
+    };
+}
+
+function click_SendResetCode()
+{
+    const sessionIDField = document.getElementById("sessionID");
+    const recoveryUsernameField = document.getElementById("recoveryUsername");
+
+    if(PerformElementFieldCheck(sessionIDField, "a", "session ID") == false) return;
+    const sessionIDparts = sessionIDField.value.split(":");
+    if(sessionIDparts.length<1 || sessionIDparts[0].length<1) {cicMSG_ShowMessage(copyrightDiv, "Invalid session ID.", copyrightMsg, 5, 0.5, 'red', 'var(--mbaInputIndigo)', 'var(--mbaDarkPurple)'); return;}
+    if(PerformElementFieldCheck(recoveryUsernameField, "your", "username") == false) return;
+
+    const recoverPasswordData = 
+    {
+        serverID: sessionIDparts[0],
+        sessionID: sessionIDparts[1],
+        sessionNum: sessionIDparts[2],
+        username: recoveryUsernameField.value
+    }
+}
+
+function click_ChangePassword()
+{
+    const sessionIDField = document.getElementById("sessionID");
+    const resetpassUsernameField = document.getElementById("resetpassUsername");
+    const resetpassResetCodeField = document.getElementById("resetpassResetCode");
+    const resetpassNewPasswordField = document.getElementById("resetpassNewPassword");
+    const resetpassConfirmNewPasswordField = document.getElementById("resetpassConfirmNewPassword");
+
+    if(PerformElementFieldCheck(sessionIDField, "a", "session ID") == false) return;
+    const sessionIDparts = sessionIDField.value.split(":");
+    if(sessionIDparts.length<1 || sessionIDparts[0].length<1) {cicMSG_ShowMessage(copyrightDiv, "Invalid session ID.", copyrightMsg, 5, 0.5, 'red', 'var(--mbaInputIndigo)', 'var(--mbaDarkPurple)'); return;}
+    if(PerformElementFieldCheck(resetpassUsernameField, "your", "username") == false) return;
+    if(PerformElementFieldCheck(resetpassResetCodeField, "your", "reset code") == false) return;
+    if(PerformElementFieldCheck(resetpassNewPasswordField, "your", "new password") == false) return;
+    if(!resetpassConfirmNewPasswordField.value) {cicMSG_ShowMessage(copyrightDiv, "Please confirm your new password.", copyrightMsg, 5, 0.5, 'red', 'var(--mbaInputIndigo)', 'var(--mbaDarkPurple)'); return;}
+    resetpassConfirmNewPasswordField.value = cicCutAndTrim(resetpassConfirmNewPasswordField.value);
+    if(resetpassConfirmNewPasswordField != resetpassNewPasswordField) {cicMSG_ShowMessage(copyrightDiv, "Passwords do not match.", copyrightMsg, 5, 0.5, 'red', 'var(--mbaInputIndigo)', 'var(--mbaDarkPurple)'); return;}
+
+    const resetPasswordData = 
+    {
+        serverID: sessionIDparts[0],
+        sessionID: sessionIDparts[1],
+        sessionNum: sessionIDparts[2],
+        username: resetpassUsernameField.value,
+        resetCode: resetpassResetCodeField.value,
+        newPassword: resetpassNewPasswordField.value,
+        confirmPassword: resetpassConfirmNewPasswordField.value
+    }
+}
---
+++ App/cic_FPS_Events&Msg.js
@@ -0,0 +1,127 @@
+function cicCutAndTrim(string) { if(string) return string.replace(/(^\s+|\s+$)/gm,''); else return ''; }
+
+//120FPS is SIMULATING at 125 FPS         (+0.008/every8ms)
+//90FPS  is SIMULATING at 90.909090rp FPS (+0.011/every11ms)
+//60FPS  is SIMULATING at 62.5 FPS        (+0.016/every16ms)
+//30FPS  is SIMULATING at 30.303030rp FPS (+0.033/every33ms)
+//24FPS  is SIMULATING at 24.39024rep FPS (+0.041/every41ms)
+//10FPS  is SIMULATING at 10 FPS          (+0.1/every100ms)
+let targetFPS = 30;
+const startTime = Object.freeze(new Date());
+const cicFPSeventType = Object.freeze({perTick:0, repeatInterval:1, delayOnce:2, exactTime:3});
+const cicFPSevents = { timerFPS:0, frameDuration:0, deltaTime:0, updateFPSevents:[] };
+let intervalID = null;
+
+SetFPSinterval(targetFPS);
+function SetFPSinterval(FPS)
+{
+    if(intervalID!=null)
+        clearInterval(intervalID);
+    targetFPS = FPS;
+    intervalID = setInterval(UpdateFPSevents, Math.floor(1000/targetFPS));
+}
+
+function cicFPS_SubscribeFPSevent(func, time, type=cicFPSeventType.perTick)
+{
+    cicFPSevents.updateFPSevents.push({func:func, time:((type==cicFPSeventType.perTick)?(cicFPS_MillisecondsSinceStart()):(time*1000)), type:type, nextTime:((type==cicFPSeventType.perTick)?(cicFPS_MillisecondsSinceStart()):((type==cicFPSevents.timedOnce)?(time*1000):(cicFPSevents.timerFPS+time*1000)))});
+}
+
+function cicFPS_UnsubscribeFPSevent(func)
+{
+    const index = cicFPSevents.updateFPSevents.findIndex(obj => obj.fn === func);
+    if (index !== -1) cicFPSevents.updateFPSevents.splice(index, 1);
+}
+
+function UpdateFPSevents()
+{
+    const currTime=cicFPS_MillisecondsSinceStart(); 
+    cicFPSevents.frameDuration=currTime-cicFPSevents.timerFPS; 
+    cicFPSevents.deltaTime=cicFPSevents.frameDuration/1000; 
+    cicFPSevents.timerFPS=currTime; 
+
+    let i=0;
+    while(i<cicFPSevents.updateFPSevents.length)
+    {
+        if(cicFPSevents.timerFPS>=cicFPSevents.updateFPSevents[i].nextTime)
+        {
+            cicFPSevents.updateFPSevents[i].func();
+            if(cicFPSevents.updateFPSevents[i].type==cicFPSeventType.repeatInterval)
+                cicFPSevents.updateFPSevents[i].nextTime += cicFPSevents.updateFPSevents[i].time;
+
+            if(cicFPSevents.updateFPSevents[i].type==cicFPSeventType.delayOnce || cicFPSevents.updateFPSevents[i].type==cicFPSeventType.exactTime)
+                cicFPSevents.updateFPSevents.splice(i, 1);
+            else
+                i++;
+        }
+        else
+        {
+            i++;
+        }
+    }
+}
+
+function cicFPS_MillisecondsSinceStart()
+{
+    return ((new Date().getTime()) - startTime.getTime()); //getTime() method of Date instances returns the number of milliseconds for this date since the epoch, which is defined as the midnight at the beginning of January 1, 1970, UTC.
+}
+
+
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
+const copyrightDiv = document.getElementById("copyrightDiv");
+const copyrightMsg = "©2024 CIC Studios";
+const copyrightColor = 'var(--mbaDarkPurple)';
+let responseDiv = copyrightDiv;
+let responseDefaultMsg = "";
+let responseDefaultColor = 'var(--mbaDarkPurple)';
+let responseBlinkColorOdd = 'var(--mbaInputIndigo)';
+let responseBlinkColorEven = 'white';
+let responseBlinkInterval = 0;
+let responseBlinkTimeout = 0;
+let responseBlinkCount = 0;
+let responseDivTimeout = 0;
+let isShowingResponseDiv = false;
+
+cicFPS_SubscribeFPSevent(UpdateMessageDiv, 0, cicFPSeventType.perTick);
+function UpdateMessageDiv()
+{
+    if(isShowingResponseDiv)
+    {
+        if(cicFPSevents.timerFPS>responseDivTimeout)
+        {
+            isShowingResponseDiv = false;
+            responseBlinkCount = 0;
+            responseDiv.innerHTML = responseDefaultMsg;
+            responseDiv.style.color = responseDefaultColor;
+        }
+        else if(responseBlinkInterval>0 && cicFPSevents.timerFPS>responseBlinkTimeout)
+        {
+            responseBlinkTimeout = cicFPSevents.timerFPS+responseBlinkInterval;
+            responseBlinkCount = (responseBlinkCount+1)%2;
+            responseDiv.style.color = (responseBlinkCount==0)?(responseBlinkColorEven):(responseBlinkColorOdd);
+        }
+    }
+}
+
+function cicMSG_ShowMessage(targetDiv, msg, defaultMsg="", duration=5, blinkInterval=0, colorEven="white", colorOdd="black", colorDefault="black")
+{
+    if(responseDiv!=targetDiv)
+    {
+        responseDiv.innerHTML = responseDefaultMsg;
+        responseDiv.style.color = responseDefaultColor;
+    }
+    responseDiv = targetDiv;
+    responseBlinkInterval = blinkInterval*1000;
+    responseBlinkTimeout = cicFPSevents.timerFPS+responseBlinkInterval;
+    responseDivTimeout = cicFPSevents.timerFPS+duration*1000;
+    responseDefaultColor = colorDefault;
+    responseBlinkColorEven = colorEven;
+    responseBlinkColorOdd = colorOdd;
+    responseDefaultMsg = defaultMsg;
+    responseBlinkCount = 0;
+
+    responseDiv.innerHTML = msg;
+    responseDiv.style.color = colorEven;
+    isShowingResponseDiv = true;
+}
